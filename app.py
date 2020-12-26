6cfdfb3c8fa1ce36d71e1916f5a971eee8fd4b3d
Emil Sadek <esadek@hotmail.com>
2020-12-26 14:07:29 -0800

    Add active states to navbar links

--- app.py
+++ app.py
@@ -7,14 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', home_active='active')
 
 @app.route('/launches')
 def launches():
     r = get('https://api.spacexdata.com/v4/launches/past')
     past_launches = loads(r.text)[::-1]
-    return render_template('launches.html', past_launches=past_launches)
+    return render_template('launches.html', launches_active='active', past_launches=past_launches)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', about_active='active')
+
+if __name__ == '__main__':
+    app.run(debug=True)
