17babdd39a6c262f2b7093c80b7e5d74a526b86e
Emil Sadek <esadek@hotmail.com>
2021-03-29 13:31:56 -0700

    Change: test name

--- test/unit/test_routing.py
+++ test/unit/test_routing.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 from application import application
 
 
-class TestApplication(unittest.TestCase):
+class TestRouting(unittest.TestCase):
 
     def setUp(self):
         self.app = application.test_client()
