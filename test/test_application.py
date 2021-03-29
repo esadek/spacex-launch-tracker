dd972ab4026fcaa4e8b231a22a55b16380f30955
Emil Sadek <esadek@hotmail.com>
2021-03-29 12:30:44 -0700

    Remove: whitespace

--- test/test_application.py
+++ test/test_application.py
@@ -11,17 +11,17 @@ class TestApplication(unittest.TestCase):
         self.app = application.test_client()
 
     def test_home_status_code(self):
-        result = self.app.get('/') 
+        result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
 
     def test_upcoming_status_code(self):
-        result = self.app.get('/upcoming') 
+        result = self.app.get('/upcoming')
         self.assertEqual(result.status_code, 200)
 
     def test_past_status_code(self):
-        result = self.app.get('/past') 
+        result = self.app.get('/past')
         self.assertEqual(result.status_code, 200)
 
     def test_about_status_code(self):
-        result = self.app.get('/about') 
+        result = self.app.get('/about')
         self.assertEqual(result.status_code, 200)
