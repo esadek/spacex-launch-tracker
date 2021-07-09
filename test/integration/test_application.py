af366ebcada7b573a98bfbf597d7b53c6649e7dd
Emil Sadek <esadek@hotmail.com>
2021-07-08 22:51:01 -0700

    Fix: wrong word errors

--- test/integration/test_application.py
+++ test/integration/test_application.py
@@ -11,13 +11,13 @@ class TestApp(unittest.TestCase):
         self.app = app.test_client()
 
     def test_home_content(self) -> None:
-        result = self.app.get('/')
-        self.assertTrue(b'Success' in result.data or b'Failure' in result.data)
+        response = self.app.get('/')
+        self.assertTrue(b'Success' in response.data or b'Failure' in response.data)
 
     def test_upcoming_content(self) -> None:
-        result = self.app.get('/upcoming')
-        self.assertTrue(b'Falcon 9' in result.data)
+        response = self.app.get('/upcoming')
+        self.assertTrue(b'Falcon 9' in response.data)
 
     def test_past_content(self) -> None:
-        result = self.app.get('/past')
-        self.assertTrue(b'Falcon 9' in result.data)
+        response = self.app.get('/past')
+        self.assertTrue(b'Falcon 9' in response.data)
--- test/unit/test_routing.py
+++ test/unit/test_routing.py
@@ -11,17 +11,17 @@ class TestRouting(unittest.TestCase):
         self.app = app.test_client()
 
     def test_home_status_code(self) -> None:
-        result = self.app.get('/')
-        self.assertEqual(result.status_code, 200)
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
 
     def test_upcoming_status_code(self) -> None:
-        result = self.app.get('/upcoming')
-        self.assertEqual(result.status_code, 200)
+        response = self.app.get('/upcoming')
+        self.assertEqual(response.status_code, 200)
 
     def test_past_status_code(self) -> None:
-        result = self.app.get('/past')
-        self.assertEqual(result.status_code, 200)
+        response = self.app.get('/past')
+        self.assertEqual(response.status_code, 200)
 
     def test_about_status_code(self) -> None:
-        result = self.app.get('/about')
-        self.assertEqual(result.status_code, 200)
+        response = self.app.get('/about')
+        self.assertEqual(response.status_code, 200)
