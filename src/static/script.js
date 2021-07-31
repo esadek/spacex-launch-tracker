3fdacf821d40ae56d4a0de5c27b2c357f422814c
Emil Sadek <esadek@hotmail.com>
2021-07-31 13:00:36 -0700

    Change: variable declaration keywords

--- src/static/script.js
+++ src/static/script.js
@@ -2,12 +2,12 @@ const countdownElement = document.getElementById('countdown')
 const launchDateUnix = countdownElement.dataset.date
 const launchDate = new Date(launchDateUnix * 1000)
 
-var countdown = setInterval(function() {
-    var currentDate = new Date()
-    var difference = launchDate - currentDate
-    var days = Math.floor(difference / (1000 * 60 * 60 * 24))
-    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000)
+let countdown = setInterval(function() {
+    let currentDate = new Date()
+    let difference = launchDate - currentDate
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24))
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000)
     countdownElement.innerHTML = days + 'd : ' + hours + 'h : ' + minutes + 'm : ' + seconds + 's'
 }, 1000)
