ec4f85e4b258552cc0e53663414802fec4c3ccd8
Emil Sadek <esadek@hotmail.com>
2021-08-09 20:56:33 -0700

    Change: whitespace

--- test/integration/test_application.py
+++ test/integration/test_application.py
@@ -1,12 +1,12 @@
 import os
 import sys
 import unittest
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 from app import app
 
 
 class TestApp(unittest.TestCase):
-
     def setUp(self) -> None:
         self.app = app.test_client()
 
