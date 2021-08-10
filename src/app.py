f599ad7c8b4d6fbc97c68bc6368c92881eb8e44f
Emil Sadek <esadek@hotmail.com>
2021-08-09 21:20:28 -0700

    Change: data import

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 from requests_cache import install_cache
 
-from data import (get_next_launch, get_latest_launch, get_upcoming_launches,
-                  get_past_launches)
+import data
 
 CACHE_DURATION_SECONDS = 3600
 install_cache(expire_after=CACHE_DURATION_SECONDS)
@@ -13,21 +12,21 @@ app = Flask(__name__)
 @app.route('/')
 def home() -> str:
     context = {
-        'next_launch': get_next_launch(),
-        'latest_launch': get_latest_launch()
+        'next_launch': data.get_next_launch(),
+        'latest_launch': data.get_latest_launch()
     }
     return render_template('index.html', context=context)
 
 
 @app.route('/upcoming')
 def upcoming() -> str:
-    context = {'upcoming_launches': get_upcoming_launches()}
+    context = {'upcoming_launches': data.get_upcoming_launches()}
     return render_template('upcoming.html', context=context)
 
 
 @app.route('/past')
 def past() -> str:
-    context = {'past_launches': get_past_launches()[::-1]}
+    context = {'past_launches': data.get_past_launches()[::-1]}
     return render_template('past.html', context=context)
 
 
